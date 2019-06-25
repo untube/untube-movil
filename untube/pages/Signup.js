56debf6a4716c584146aee2e20c95343e9ea44ed
Stefania <sarguellesr>
2019-06-24 23:03:14 -0500

    login, signup

---
+++ untube/pages/Signup.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import{
+    StyleSheet,
+    Text,
+    View,
+    StatusBar
+} from 'react-native';
+
+import Form from '../components/Form';
+
+export default class Signup extends React.Component {
+    render(){
+        return(
+            <View style = {styles.container}>
+                <Text>Login</Text>
+                <Form type = "Registrarse"/>
+                <View style = {styles.signupTextCont}>
+                    <Text style = {styles.signupText}>¿No tienes una cuenta?</Text>
+                    <Text style = {styles.signupButton}>Registrate!</Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: '#455a64',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    signupTextCont: {
+        flexGrow: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        paddingVertical: 16,
+        flexDirection: 'row'   
+    },
+    signupText: {
+        color: 'rgba(255,255,255,0.7)',
+        fontSize: 16
+    },
+    signupButton: {
+        color :'#ffffff',
+        fontSize: 16,
+        fontWeight: '500'
+    }
+});
